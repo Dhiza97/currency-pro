bb4292291ee146e2d1f11b510be88879b861f497
Joseph Garba <josephgarba97@gmail.com>
2026-05-16 13:39:08 +0100

    Add Google OAuth callback route to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
+import AuthCallback from "./pages/AuthCallback";
 
 function App() {
   const { getAccessToken, refreshAccessToken } = useContext(AuthContext);
@@ -24,6 +25,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Dashboard />} />
+          <Route path="/auth/callback" element={<AuthCallback />} />
         </Routes>
       </BrowserRouter>
     </>
